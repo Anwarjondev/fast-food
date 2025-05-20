be34484fef200d63fdb6d9e1f85b4c29adde7a0c
Anwarjondev <anvarjonturdaliyev66@gmail.com>
2025-05-20 10:28:02 +0500

    fix the CORS error

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Anwarjondev/fast-food/internal/db"
 	"github.com/Anwarjondev/fast-food/internal/handlers"
 	"github.com/Anwarjondev/fast-food/internal/middleware"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"     // swagger embed files
 	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
@@ -38,6 +39,9 @@ func main() {
 	handlers.SetConfig(cfg)
 	r := gin.Default()
 	r.Use(gin.Logger(), gin.Recovery())
+
+	// Add CORS middleware
+	r.Use(cors.Default())
 
 	// Swagger documentation
 	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
